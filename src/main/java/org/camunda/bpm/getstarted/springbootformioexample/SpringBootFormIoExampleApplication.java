1b6b07f279a2a6ed3ed75ae3d1631be95402dc3a
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-30 12:46:23 +0330

    spring boot app initialized . added some deps to pom .xml file !

---
+++ src/main/java/org/camunda/bpm/getstarted/springbootformioexample/SpringBootFormIoExampleApplication.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.springbootformioexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormIoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormIoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/camunda/bpm/getstarted/springbootformioexample/SpringBootFormIoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.springbootformioexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormIoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
