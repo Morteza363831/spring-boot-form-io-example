11ee626b3db419df3e68848c0dc051bca88118cd
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-31 13:16:58 +0330

    before it FormController was using Controller notation but now this is based on RestController notation to transfer data between client and server using json ! added form end point to display form.html to user and get input from user ! form.html has been rendered using a json file has been retrieved from form io uri

--- src/main/java/org/camunda/bpm/getstarted/springbootformioexample/controller/FormController.java
+++ src/main/java/org/camunda/bpm/getstarted/springbootformioexample/controller/FormController.java
@@ -1,27 +1,93 @@
 package org.camunda.bpm.getstarted.springbootformioexample.controller;
 
+import ch.qos.logback.classic.Logger;
+import jakarta.servlet.http.HttpSession;
 import org.camunda.bpm.getstarted.springbootformioexample.service.FormService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/forms")
 public class FormController {
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(FormController.class);
+
+    private final FormService formService;
+    private final ResourceLoader resourceLoader;
+
+    public FormController(FormService formService, ResourceLoader resourceLoader) {
+        this.formService = formService;
+        this.resourceLoader = resourceLoader;
+    }
+
+    @GetMapping("/rest/form")
+    public ResponseEntity<String> restForm() throws IOException {
+        String formDefinition = formService.getFormObject();
+
+        // Log the form definition
+        logger.info("Form Definition: {}", formDefinition);
+
+        // Escape double quotes in JSON string to safely embed in HTML
+        String escapedFormDefinition = formDefinition.replace("\"", "\\\"");
+
+        Resource resource = resourceLoader.getResource("classpath:templates/form.html");
+        String content = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+
+        // Inject the escaped form definition into the HTML template
+        content = content.replace("/*[[${formDefinition}]]*/ '{}'", "\"" + escapedFormDefinition + "\"");
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, "text/html")
+                .body(content);
+    }
+
+
+
     @GetMapping("/form")
-    public String form() {
-        return "formio.html";
+    public ResponseEntity<Resource> form() {
+        String RESOURCE = "classpath:templates/formio.html";
+        Resource resource = resourceLoader.getResource(RESOURCE);
+        if (resource.exists()) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE,"text/html")
+                    .body(resource);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/form/complete")
-    public String completeForm(@RequestBody Map<String, String> formData) {
+    public ResponseEntity<Map<String, String>> completeForm(@RequestBody Map<String, String> formData, HttpSession session) {
+        session.setAttribute("formData", formData);
         System.out.println(formData.get("name") + " " + formData.get("password"));
-        return "redirect:/api/forms/form";
+        return ResponseEntity.ok(Map.of("name",formData.get("name"),"password",formData.get("password")));
+    }
+
+    @GetMapping("/result")
+    public ResponseEntity<Resource> result(@RequestParam("name") String name,
+                                           @RequestParam("password") String password) {
+        String RESOURCE = "classpath:templates/result.html";
+        Resource resource = resourceLoader.getResource(RESOURCE);
+
+        if (resource.exists()) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE,"text/html")
+                    .body(resource);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
