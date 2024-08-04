bfae135b54b089b22080b191ffd17f2f0c961871
ruzstock.ir <ali16mar15couples@gmail.com>
2024-08-04 16:50:20 +0330

    comments added . then the code is clean now . unusable sections commented

--- src/main/java/org/camunda/bpm/getstarted/springbootformioexample/controller/FormController.java
+++ src/main/java/org/camunda/bpm/getstarted/springbootformioexample/controller/FormController.java
@@ -3,6 +3,7 @@ package org.camunda.bpm.getstarted.springbootformioexample.controller;
 import ch.qos.logback.classic.Logger;
 import jakarta.servlet.http.HttpSession;
 import org.camunda.bpm.getstarted.springbootformioexample.service.FormService;
+import org.camunda.bpm.getstarted.springbootformioexample.service.SendRequestService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -10,13 +11,10 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -25,19 +23,24 @@ import java.util.Map;
 @RequestMapping("/api/forms")
 public class FormController {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     private static final Logger logger = (Logger) LoggerFactory.getLogger(FormController.class);
 
-    private final FormService formService;
+
+    private final RestTemplate restTemplate;
+    private final SendRequestService sendRequestService;
     private final ResourceLoader resourceLoader;
 
-    public FormController(FormService formService, ResourceLoader resourceLoader) {
-        this.formService = formService;
+    public FormController(RestTemplate restTemplate,
+                          SendRequestService sendRequestService,
+                          ResourceLoader resourceLoader) {
+        this.restTemplate = restTemplate;
+        this.sendRequestService = sendRequestService;
         this.resourceLoader = resourceLoader;
     }
 
+
+    /*// display front file for fluid simulation ...
+    // there is no relation between this and project
     @GetMapping("/file")
     public ResponseEntity<Resource> file() {
         String RESOURCE = "classpath:templates/file.html";
@@ -52,8 +55,10 @@ public class FormController {
         }
     }
 
+    // this endpoint get json file from form io then render it and display in html file
     @GetMapping("/rest/form")
     public ResponseEntity<String> restForm() throws IOException {
+        // get json base of form from uri (form io)
         String formDefinition = formService.getFormObject();
 
         // Log the form definition
@@ -62,23 +67,30 @@ public class FormController {
         // Escape double quotes in JSON string to safely embed in HTML
         String escapedFormDefinition = formDefinition.replace("\"", "\\\"");
 
+        // find html resource
         Resource resource = resourceLoader.getResource("classpath:templates/form.html");
+
+        // select content of html file
         String content = new String(Files.readAllBytes(Paths.get(resource.getURI())));
 
         // Inject the escaped form definition into the HTML template
-        content = content.replace("/*[[${formDefinition}]]*/ '{}'", "\"" + escapedFormDefinition + "\"");
+        content = content.replace("[[${formDefinition}]] '{}'", "\"" + escapedFormDefinition + "\"");
 
+        // return response object with headers and html file and the updated content
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, "text/html")
                 .body(content);
     }
 
 
-
+    // this form is like upper ! but
+    // here we have static json form ! we did not get the form from uri !
     @GetMapping("/form")
     public ResponseEntity<Resource> form() {
+        // get resource
         String RESOURCE = "classpath:templates/formio.html";
         Resource resource = resourceLoader.getResource(RESOURCE);
+        // check the resource is correct
         if (resource.exists()) {
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_TYPE,"text/html")
@@ -89,20 +101,23 @@ public class FormController {
         }
     }
 
+    // submit form using post endpoint
     @PostMapping("/form/complete")
     public ResponseEntity<Map<String, String>> completeForm(@RequestBody Map<String, String> formData, HttpSession session) {
-        session.setAttribute("formData", formData);
-        System.out.println(formData.get("name") + " " + formData.get("password"));
+        // return response object with status a map body
         return ResponseEntity.ok(Map.of("name",formData.get("name"),"password",formData.get("password")));
     }
 
     @GetMapping("/result")
     public ResponseEntity<Resource> result(@RequestParam("name") String name,
                                            @RequestParam("password") String password) {
+        // find resource
         String RESOURCE = "classpath:templates/result.html";
         Resource resource = resourceLoader.getResource(RESOURCE);
 
+        // check it
         if (resource.exists()) {
+            // return response object
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_TYPE,"text/html")
                     .body(resource);
@@ -112,49 +127,46 @@ public class FormController {
         }
     }
 
+    */
 
+
+    // display user form to user
     @GetMapping("/user/form")
     public ResponseEntity<Resource> userForm() {
+        // find resource
         String RESOURCE = "classpath:templates/userForm.html";
         Resource resource = resourceLoader.getResource(RESOURCE);
+        //  check the resource is correct or no
         if (resource.exists()) {
+            // return response object
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_TYPE,"text/html")
                     .body(resource);
         }
         return ResponseEntity.notFound().build();
     }
+    // retrieve data after submitting and make a process
     @PostMapping("/user/form/complete")
     public ResponseEntity<Map<String,Object>> completeUserForm(@RequestBody Map<String, Object> formData, HttpSession session) {
-        String url = "http://localhost:8090/submit-form";
 
-        // Create headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        // Create the HttpEntity with form data and headers
-        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(formData, headers);
-
-        // Make a POST request to the first project's /submit-form endpoint
-        ResponseEntity<Map> responseEntity = restTemplate.postForEntity(url, requestEntity, Map.class);
-
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            Map<String, Object> updatedData = responseEntity.getBody();
+        String destinationUrl = "http://localhost:8090/submit-form";
+        ResponseEntity<Map> response = sendRequestService.sendRequest(destinationUrl,formData);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            Map<String, Object> updatedData = (Map<String, Object>) response.getBody();
             return ResponseEntity.ok(updatedData);
         } else {
-            return ResponseEntity.status(responseEntity.getStatusCode()).body(null);
+            return ResponseEntity.status(response.getStatusCode()).body(null);
         }
     }
 
 
     @GetMapping("/user/display")
-    public ResponseEntity<Resource> displayUser(@RequestParam String userName, @RequestParam Integer userAge, @RequestParam String userJob) {
+    public ResponseEntity<Resource> displayUser(HttpSession session) {
 
         String RESOURCE = "classpath:templates/displayUser.html";
         Resource resource = resourceLoader.getResource(RESOURCE);
 
         if (resource.exists()) {
-            System.out.println(userName);
             // Replace placeholders in the HTML template with actual data
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_TYPE, "text/html")
